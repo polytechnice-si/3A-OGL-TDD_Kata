bb1b3b0e36eb2022702892140c004bdf25d9b804
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 15:09:19 +0200

    Rectangle support for translation(initial)

--- src/main/java/fr/unice/polytech/ogl/tdd/Rectangle.java
+++ src/main/java/fr/unice/polytech/ogl/tdd/Rectangle.java
@@ -3,6 +3,18 @@ package fr.unice.polytech.ogl.tdd;
 
 public class Rectangle {
 
-	public Rectangle(int x1, int y1, int x2, int y2) {}
+	private int x1, y1, x2, y2;
+
+	public Rectangle(int x1, int y1, int x2, int y2) {
+		this.x1 = x1; this.y1 = y1;
+		this.x2 = x2; this.y2 = y2;
+	}
+
+	public int getX1() { return x1;	}
+	public int getY1() { return y1;	}
+	public int getX2() { return x2;	}
+	public int getY2() { return y2; }
+
+	public void translate(int deltaX, int deltaY) {	}
 
 }
