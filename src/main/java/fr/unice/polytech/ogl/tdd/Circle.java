f9c5f8c49046912f5f2b1f492ffa81018d77b63e
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 14:21:04 +0200

    specifications for Rectangle and Circle instantiation

---
+++ src/main/java/fr/unice/polytech/ogl/tdd/Circle.java
@@ -0,0 +1,5 @@
+package fr.unice.polytech.ogl.tdd;
+
+
+public class Circle {
+}
--- src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
+++ src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
@@ -15,11 +15,11 @@ public class HotDrawTest {
 	@Test
 	public void testCanvas() {
 		Canvas c = new Canvas();
-		c.add(new Circle());
+		c.add(new Circle(10, 20, 5));    // Circle located at (10,20), with radius 5.
 		assertEquals(1, c.getNumberOfElements());
 
-		c.add(new Circle());
-		c.add(new Rectangle());
+		c.add(new Circle());             // Default circle, located at (0,0) with radius 1
+		c.add(new Rectangle(5,6,10,8));  // Rectangle with one corner at (5,6) and the second at (10,8)
 		assertEquals(3, c.getNumberOfElements());
 	}
 
