0a1f63a3dd7bd457ea06a2d8ec1ba1fe1fbd02b2
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 13:59:15 +0200

    initial version of Canvas (only support for empty canvas)

---
+++ src/main/java/fr/unice/polytech/ogl/tdd/Canvas.java
@@ -0,0 +1,14 @@
+package fr.unice.polytech.ogl.tdd;
+
+/**
+ * This file is part of the tdd-kata project
+ *
+ * @author mosser (12/04/2015, 13:54)
+ */
+public class Canvas {
+
+	public int getNumberOfElements() {
+		return 0;
+	}
+
+}
