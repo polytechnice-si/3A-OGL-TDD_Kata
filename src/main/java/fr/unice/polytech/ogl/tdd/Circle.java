7835b0b4ff81b04d32d9170ce433836791932ae4
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 14:23:13 +0200

    Circle and rectangle compliant with the specification

--- src/main/java/fr/unice/polytech/ogl/tdd/Circle.java
+++ src/main/java/fr/unice/polytech/ogl/tdd/Circle.java
@@ -2,4 +2,9 @@ package fr.unice.polytech.ogl.tdd;
 
 
 public class Circle {
+
+	public Circle() {}
+
+	public Circle(int x, int y, int r) {}
+
 }
--- src/main/java/fr/unice/polytech/ogl/tdd/Rectangle.java
+++ src/main/java/fr/unice/polytech/ogl/tdd/Rectangle.java
@@ -2,4 +2,7 @@ package fr.unice.polytech.ogl.tdd;
 
 
 public class Rectangle {
+
+	public Rectangle(int x1, int y1, int x2, int y2) {}
+
 }
