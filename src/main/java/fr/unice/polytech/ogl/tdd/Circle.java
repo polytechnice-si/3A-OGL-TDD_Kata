23fee340ece0cdd46dd5fe19f8395966b5813234
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 14:51:59 +0200

    Circle support for translation (initial)

--- src/main/java/fr/unice/polytech/ogl/tdd/Circle.java
+++ src/main/java/fr/unice/polytech/ogl/tdd/Circle.java
@@ -7,4 +7,9 @@ public class Circle {
 
 	public Circle(int x, int y, int r) {}
 
+	public int getX() { return 0; }
+	public int getY() { return 0; }
+
+	public void translate(int deltaX, int deltaY) { }
+
 }
