3e5539112fa270b839d17e9725b73f77cbd4450d
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 14:32:46 +0200

    refactoring c into emptyCanvas for readability purpose

--- src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
+++ src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
@@ -7,26 +7,26 @@ import static org.junit.Assert.*;
 
 public class HotDrawTest {
 
-	private Canvas c;
+	private Canvas emptyCanvas;
 
 	@Before
 	public void initialize() {
-		c = new Canvas();
+		emptyCanvas = new Canvas();
 	}
 
 	@Test
 	public void testEmptyCanvas() {
-		assertEquals(0, c.getNumberOfElements());
+		assertEquals(0, emptyCanvas.getNumberOfElements());
 	}
 
 	@Test
 	public void testCanvas() {
-		c.add(new Circle(10, 20, 5));    // Circle located at (10,20), with radius 5.
-		assertEquals(1, c.getNumberOfElements());
+		emptyCanvas.add(new Circle(10, 20, 5));    // Circle located at (10,20), with radius 5.
+		assertEquals(1, emptyCanvas.getNumberOfElements());
 
-		c.add(new Circle());             // Default circle, located at (0,0) with radius 1
-		c.add(new Rectangle(5,6,10,8));  // Rectangle with one corner at (5,6) and the second at (10,8)
-		assertEquals(3, c.getNumberOfElements());
+		emptyCanvas.add(new Circle());             // Default circle, located at (0,0) with radius 1
+		emptyCanvas.add(new Rectangle(5, 6, 10, 8));  // Rectangle with one corner at (5,6) and the second at (10,8)
+		assertEquals(3, emptyCanvas.getNumberOfElements());
 	}
 
 }
