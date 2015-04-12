fccfd08e9d9bae844fb97cc289cdf648d415f06a
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-04-12 15:53:48 +0200

    test refactored for better understanding

--- src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
+++ src/test/java/fr/unice/polytech/ogl/tdd/HotDrawTest.java
@@ -8,10 +8,19 @@ import static org.junit.Assert.*;
 public class HotDrawTest {
 
 	private Canvas emptyCanvas;
+	private Group emptyGroup;
+	private Group group;
+	private Circle aCircle;
+	private Rectangle aRectangle;
 
 	@Before
 	public void initialize() {
 		emptyCanvas = new Canvas();
+		emptyGroup = new Group();
+		group = new Group();
+		aCircle = new Circle();                  // Default circle, located at (0,0) with radius 1
+		aRectangle = new Rectangle(5, 6, 10, 8); // Rectangle with one corner at (5,6) and the second at (10,8)
+		group.add(aCircle); group.add(aRectangle);
 	}
 
 	@Test
@@ -24,69 +33,60 @@ public class HotDrawTest {
 		emptyCanvas.add(new Circle(10, 20, 5));    // Circle located at (10,20), with radius 5.
 		assertEquals(1, emptyCanvas.getNumberOfElements());
 
-		emptyCanvas.add(new Circle());             // Default circle, located at (0,0) with radius 1
-		emptyCanvas.add(new Rectangle(5, 6, 10, 8));  // Rectangle with one corner at (5,6) and the second at (10,8)
+		emptyCanvas.add(aCircle);
+		emptyCanvas.add(aRectangle);
 		assertEquals(3, emptyCanvas.getNumberOfElements());
 	}
 
 	@Test
 	public void translatingCircle() {
-		Circle c = new Circle();
-		int oldX = c.getX();
-		int oldY = c.getY();
+		int oldX = aCircle.getX();
+		int oldY = aCircle.getY();
 
-		c.translate(2, 3);
-		assertEquals(oldX + 2, c.getX());
-		assertEquals(oldY + 3, c.getY());
+		aCircle.translate(2, 3);
+		assertEquals(oldX + 2, aCircle.getX());
+		assertEquals(oldY + 3, aCircle.getY());
 	}
 
 	@Test
 	public void translatingRectangle() {
-		Rectangle r = new Rectangle(5, 6, 10, 8);
-		int oldX1 = r.getX1();
-		int oldY1 = r.getY1();
-		int oldX2 = r.getX2();
-		int oldY2 = r.getY2();
+		int oldX1 = aRectangle.getX1();
+		int oldY1 = aRectangle.getY1();
+		int oldX2 = aRectangle.getX2();
+		int oldY2 = aRectangle.getY2();
 
-		r.translate(2, 3);
-		assertEquals(oldX1 + 2, r.getX1());
-		assertEquals(oldY1 + 3, r.getY1());
-		assertEquals(oldX2 + 2, r.getX2());
-		assertEquals(oldY2 + 3, r.getY2());
+		aRectangle.translate(2, 3);
+		assertEquals(oldX1 + 2, aRectangle.getX1());
+		assertEquals(oldY1 + 3, aRectangle.getY1());
+		assertEquals(oldX2 + 2, aRectangle.getX2());
+		assertEquals(oldY2 + 3, aRectangle.getY2());
 	}
 
 	@Test
 	public void translatingCanvas() {
-		Circle c = new Circle();
-		int cOldX = c.getX();
-		int cOldY = c.getY();
+		int cOldX = aCircle.getX();
+		int cOldY = aCircle.getY();
+		int rOldX1 = aRectangle.getX1();
+		int rOldY1 = aRectangle.getY1();
+		int rOldX2 = aRectangle.getX2();
+		int rOldY2 = aRectangle.getY2();
 
-		Rectangle r = new Rectangle(5, 6, 10, 8);
-		int rOldX1 = r.getX1();
-		int rOldY1 = r.getY1();
-		int rOldX2 = r.getX2();
-		int rOldY2 = r.getY2();
-
-		emptyCanvas.add(c);
-		emptyCanvas.add(r);
+		emptyCanvas.add(aCircle);
+		emptyCanvas.add(aRectangle);
 		emptyCanvas.translate(2, 3);
 
-		assertEquals(cOldX + 2, c.getX());
-		assertEquals(cOldY + 3, c.getY());
-		assertEquals(rOldX1 + 2, r.getX1());
-		assertEquals(rOldY1 + 3, r.getY1());
-		assertEquals(rOldX2 + 2, r.getX2());
-		assertEquals(rOldY2 + 3, r.getY2());
+		assertEquals(cOldX + 2, aCircle.getX());
+		assertEquals(cOldY + 3, aCircle.getY());
+		assertEquals(rOldX1 + 2, aRectangle.getX1());
+		assertEquals(rOldY1 + 3, aRectangle.getY1());
+		assertEquals(rOldX2 + 2, aRectangle.getX2());
+		assertEquals(rOldY2 + 3, aRectangle.getY2());
 	}
 
 	@Test
 	public void groupingWidgets() {
-		Group g = new Group();
-		assertEquals(0, g.getNumberOfElements());
-
-		g.add(new Circle());
-		g.add(new Rectangle(5, 6, 10, 8));
-		assertEquals(2, g.getNumberOfElements());
+		assertEquals(0, emptyGroup.getNumberOfElements());
+		assertEquals(2, group.getNumberOfElements());
 	}
 
 	@Test
@@ -94,7 +94,7 @@ public class HotDrawTest {
 		Group g = new Group();
 		g.add(new Circle());
 		g.add(new Rectangle(5, 6, 10, 8));
-		g.translate(2,3);
+		g.translate(2, 3);
 		// will be tricky to write ...
 	}
 
